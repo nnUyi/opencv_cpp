374f1e5f22f58b8fd394f8334df0eaa726baedee
Youzhao Yang <yzyang17@fudan.edu.cn>
2020-08-25 15:54:22 +0800

    Update is_cam_trans.h

--- is_cam_trans.h
+++ is_cam_trans.h
@@ -9,23 +9,26 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/core/core.hpp>
-
 #include <string.h>
+
 using namespace cv;
 
 class CameraDetector {
  public:
    // 构造函数
-   CameraDetector();
+   CameraDetector() {}
    // 析构函数
-   ~CameraDetector();
+   ~CameraDetector() {}
 
  public:
-   bool is_cam_trans(String cap_path);
-   bool abs_frame(Mat queue_gray);
-   Mat crop_image_top(Mat img);
-   Mat crop_image_bottom(Mat img);
-   Mat resize_image(Mat img);
-   Mat image_preprocess(Mat img);
-   bool sift_detection(Mat img, Mat kp_img, Mat des_img, Mat rect_top, Mat rect_bottom);
+   void is_cam_trans(char* cap_path);
+   void is_cam_trans_re(Mat cur_frame, Mat pre_frame);
+   // 帧差法
+   bool abs_frame(Mat cur_frame, Mat pre_frame, bool &is_trans, Mat& mask, float &percent);
+   void crop_image_top(Mat img, Point rook_points[1][4]);
+   void crop_image_bottom(Mat img, Point rook_points[1][4]);
+   void resize_image(Mat& img);
+   // 图像预处理
+   void image_preprocess(Mat& img, Point top_rook_points[1][4], Point bottom_rook_points[1][4]);
+   bool sift_detection(Mat img, Mat kp_img, Mat& des_img, Mat rect_top, Mat rect_bottom); 
 };
